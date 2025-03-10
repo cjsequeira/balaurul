96873b10c3370cec808edde96e90541d4e28df4f
Chris Musei-Sequeira <63619139+cjsequeira@users.noreply.github.com>
2025-03-10 16:21:54 -0400

    Changed spelling of 2's Comp in UI

--- src/index.js
+++ src/index.js
@@ -7,6 +7,17 @@ import * as ModuleUtil from "./util.js";
 
 // **** CONSTANTS
 // Web page user interface
+const UI_PC_BINARY = document.getElementById("app_12bit_pc_binary");
+const UI_PC_OCTAL = document.getElementById("app_12bit_pc_octal");
+const UI_PC_HEX = document.getElementById("app_12bit_pc_hex");
+const UI_PC_DEC = document.getElementById("app_12bit_pc_dec");
+
+const UI_IR_BINARY = document.getElementById("app_12bit_ir_binary");
+const UI_IR_OCTAL = document.getElementById("app_12bit_ir_octal");
+const UI_IR_HEX = document.getElementById("app_12bit_ir_hex");
+const UI_IR_DEC = document.getElementById("app_12bit_ir_dec");
+const UI_IR_MNEMONIC = document.getElementById("app_12bit_ir_mnemonic");
+
 const UI_A_BINARY = document.getElementById("app_12bit_a_binary");
 const UI_A_OCTAL = document.getElementById("app_12bit_a_octal");
 const UI_A_HEX = document.getElementById("app_12bit_a_hex");
@@ -19,32 +30,23 @@ const UI_B_HEX = document.getElementById("app_12bit_b_hex");
 const UI_B_DEC = document.getElementById("app_12bit_b_dec");
 const UI_B_2S_COMP_DEC = document.getElementById("app_12bit_b_2s_comp_dec");
 
-const UI_PC_BINARY = document.getElementById("app_12bit_pc_binary");
-const UI_PC_OCTAL = document.getElementById("app_12bit_pc_octal");
-const UI_PC_HEX = document.getElementById("app_12bit_pc_hex");
-const UI_PC_DEC = document.getElementById("app_12bit_pc_dec");
-const UI_PC_MNEMONIC = document.getElementById("app_12bit_pc_mnemonic");
-
 const UI_MEM = document.getElementById("app_12bit_memory");
 const UI_MEM_CELL_ID_PREFIX = "app_12bit_memcell_";
 const UI_MEM_PC_CLASS = "mem_pc";
 const UI_MEM_ROWS = 8;
 const UI_MEM_COLS = 8;
 
-const UI_DIFF_COLOR = "rgba(255, 0, 0, 1.0)";
+// const UI_DIFF_COLOR = "rgba(255, 0, 0, 1.0)";
 
 
 // **** NON-CONSTANTS
 // CPU
 var cpu = new ModuleCPU.CPU();
 
-// App and UI status
-var ready = false;
-
 
 // **** MAIN CODE
 window.addEventListener("load", setup());
-requestAnimationFrame(appUpdate);
+;
 
 
 // **** FUNCTIONS
@@ -81,6 +83,17 @@ function setup() {
 
     // **** INITIALIZE UI
     // show register values
+    UI_PC_BINARY.innerHTML = cpu.pc.toString(2).padStart(12, "0").replace(/\d{3}(?=.)/g, '$& ');
+    UI_PC_OCTAL.innerHTML = cpu.pc.toString(8).padStart(4, "0");
+    UI_PC_HEX.innerHTML = cpu.pc.toString(16).padStart(3, "0").toUpperCase();
+    UI_PC_DEC.innerHTML = cpu.pc.toString(10).padStart(4, "0");
+    
+    UI_IR_BINARY.innerHTML = cpu.ir.toString(2).padStart(12, "0").replace(/\d{3}(?=.)/g, '$& ');
+    UI_IR_OCTAL.innerHTML = cpu.ir.toString(8).padStart(4, "0");
+    UI_IR_HEX.innerHTML = cpu.ir.toString(16).padStart(3, "0").toUpperCase();
+    UI_IR_DEC.innerHTML = cpu.ir.toString(10).padStart(4, "0");
+    UI_IR_MNEMONIC.innerHTML = cpu.getMnemonic();
+    
     UI_A_BINARY.innerHTML = cpu.a.toString(2).padStart(12, "0").replace(/\d{3}(?=.)/g, '$& ');
     UI_A_OCTAL.innerHTML = cpu.a.toString(8).padStart(4, "0");
     UI_A_HEX.innerHTML = cpu.a.toString(16).padStart(3, "0").toUpperCase();
@@ -93,43 +106,38 @@ function setup() {
     UI_B_DEC.innerHTML = cpu.b.toString(10).padStart(4, "0");
     UI_B_2S_COMP_DEC.innerHTML = ModuleUtil.twosComplement(cpu.b, 12).toString(10);
 
-    UI_PC_BINARY.innerHTML = cpu.pc.toString(2).padStart(12, "0").replace(/\d{3}(?=.)/g, '$& ');
-    UI_PC_OCTAL.innerHTML = cpu.pc.toString(8).padStart(4, "0");
-    UI_PC_HEX.innerHTML = cpu.pc.toString(16).padStart(3, "0").toUpperCase();
-    UI_PC_DEC.innerHTML = cpu.pc.toString(10).padStart(4, "0");
-    UI_PC_MNEMONIC.innerHTML = cpu.getMnemonic();
-
     // format memory block UI with structure
     UI_MEM.innerHTML = memory_html;
 
     // draw a box around the memory cell pointed to by the PC
-    document.getElementById(UI_MEM_CELL_ID_PREFIX + cpu.pc.toString(10)).classList.add("pc");
+    document.getElementById(UI_MEM_CELL_ID_PREFIX + cpu.getAddressFromPC().toString(10))
+        .classList
+        .add("pc");
 
-
-    // **** MARK APP READY FOR USE
-    ready = true;
+    // **** ESTABLISH APP UPDATE CALLBACK
+    requestAnimationFrame(appUpdate);
 }
 
 // app update callback for requestAnimationFrame()
 function appUpdate() {
-    // if app is ready for use...
-    if (ready) {
-        // **** UPDATE UI ELEMENTS NEEDING UPDATE
+    if (cpu.changed) {
+        // has something in the CPU changed?
+
         // is the PC now pointing to a different UI memory element from what is boxed?
         if (cpu.pc != cpu.old_pc) {
             // unbox old UI memory element
-            document.getElementById(UI_MEM_CELL_ID_PREFIX + cpu.old_pc.toString(10))
+            document.getElementById(UI_MEM_CELL_ID_PREFIX + cpu.getAddressFromOldPC().toString(10))
                 .classList
                 .remove(UI_MEM_PC_CLASS);
 
             // box new UI memory element
-            document.getElementById(UI_MEM_CELL_ID_PREFIX + cpu.pc.toString(10))
+            document.getElementById(UI_MEM_CELL_ID_PREFIX + cpu.getAddressFromPC().toString(10))
                 .classList
                 .add(UI_MEM_PC_CLASS);
-
-            // **** SYNCHRONIZE OLD AND NEW CPU COMPONENT VALUES
-            cpu.old_pc = cpu.pc;
         }
+
+        // **** SYNCHRONIZE OLD AND NEW CPU COMPONENT VALUES       
+        cpu.syncOldAndNew();
     }
 
 
